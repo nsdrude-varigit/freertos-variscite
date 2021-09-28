e5733db8b703e9a728913a50dda12bfcbb5ded4a
FrancescoFerraro <francesco.f@variscite.com>
2021-09-28 19:38:08 +0200

    visual studio code: initial commit to support build and debug
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

---
+++ var_add_vscode_support.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# -e  Exit immediately if a command exits with a non-zero status.
+set -e
+
+SCRIPT_NAME=${0##*/}
+BSP_BASE_DIR=$PWD
+PATH_TO_ARM_TOOLCHAIN="${BSP_BASE_DIR}/../gcc-arm-none-eabi-9-2020-q2-update"
+
+usage()
+{
+	echo
+	echo "This script add .vscode folder to add visual studio code support to freertos-variscite examples"
+	echo
+	echo " Usage: $0 OPTIONS"
+	echo
+	echo " OPTIONS:"
+	echo " -b <dart_mx8mq>				board folder (DART-MX8M)."
+	echo " -d <GDBServer folder>"
+	echo " -e <freertos example folder>		example folder where add .vscode>"
+	echo " -r <executable name>			read .elf name in the CMakeLists.txt file from armgcc folder "
+	echo " -t <tcm/ddr>				ram target"
+	echo
+	echo "hello_world example: ./var_add_vscode_support.sh -b dart_mx8mq -d /opt/SEGGER/JLink_Linux_V754c_x86_64 -e boards/dart_mx8mq/multicore_examples/rpmsg_lite_pingpong_rtos/linux_remote -r hello_world.elf -t ddr"
+
+}
+
+check_params()
+{
+	if [[ ! -d boards/$BOARD_DIR ]] ; then
+		echo "ERROR: \"boards/$BOARD_DIR\" does not exist"
+		exit 1
+	fi
+
+	if [[ ! -d $GDBSERVER_DIR ]] ; then
+		echo "ERROR: \"$GDBSERVER_DIR\" does not exist"
+		exit 1
+	fi
+
+	if [[ ! -d $PATH_TO_EXAMPLE_SRC ]] ; then
+		echo "ERROR: \"$PATH_TO_EXAMPLE_SRC\" does not exist"
+		exit 1
+	fi
+
+	if [[ $RAM_TARGET != "tcm" && $RAM_TARGET != "ddr" ]]; then
+		echo "ERROR: \"$RAM_TARGET\" does not exist"
+		exit 1
+	fi
+
+	if [[ ! -d $PATH_TO_ARM_TOOLCHAIN ]] ; then
+		echo "ERROR: \"$PATH_TO_ARM_TOOLCHAIN\" does not exist"
+		exit 1
+	fi
+}
+
+make_vscode()
+{
+	case $BOARD_DIR in
+	dart_mx8mq)
+		FREE_RTOS_DEVICE_DIR="MIMX8MQ6"
+		SOC_INCLUDE_PATH="${BSP_BASE_DIR}/devices/${FREE_RTOS_DEVICE_DIR}"
+		CM_DEVICE_ID="MIMX8MQ6_M4"
+		PATH_TO_JLINKSCRIPT=iMX8M/NXP_iMX8M_Connect_CortexM4.JLinkScript
+		SVD_FILE_NAME=MIMX8MQ6_cm4
+		CORTEX_M_CPU=cortex-m4
+		;;
+	esac
+	
+	# build target
+	if [[ $RAM_TARGET == "tcm" ]] ; then
+		BUILD_TARGET="debug"
+	else
+		BUILD_TARGET="ddr_debug"
+	fi
+
+	# get executable output path
+	#if [[ -f $PATH_TO_EXAMPLE_SRC/CMakeLists.txt ]] ; then
+	#else
+	#	echo "ERROR: CMakeLists.txt not found"
+	#	exit 1
+	#fi
+
+	# cp vscode templates to example source folder
+	if [[ -d $PATH_TO_EXAMPLE_SRC/.vscode ]] ; then
+		rm -r $PATH_TO_EXAMPLE_SRC/.vscode
+	fi
+	mkdir $PATH_TO_EXAMPLE_SRC/.vscode
+	cp -r var-vscode-template/* $PATH_TO_EXAMPLE_SRC/.vscode
+
+	# cp svd file
+	cp $SOC_INCLUDE_PATH/$SVD_FILE_NAME.xml $PATH_TO_EXAMPLE_SRC/.vscode/$SVD_FILE_NAME.svd
+
+	# adjust settings.json
+	sed -i "s|t_path_to_arm_gcc_dir|$PATH_TO_ARM_TOOLCHAIN|g" "$PATH_TO_EXAMPLE_SRC/.vscode/settings.json"
+	sed -i "s|t_path_to_gdb_server_dir|$GDBSERVER_DIR|g" "$PATH_TO_EXAMPLE_SRC/.vscode/settings.json"
+	sed -i "s|t_path_to_soc_include|$SOC_INCLUDE_PATH|g" "$PATH_TO_EXAMPLE_SRC/.vscode/settings.json"
+	sed -i "s|t_build_target|$BUILD_TARGET|g" "$PATH_TO_EXAMPLE_SRC/.vscode/settings.json"
+
+	# adjust launch.json
+	sed -i "s|t_path_to_arm_toolchain|$PATH_TO_ARM_TOOLCHAIN/bin|g" "$PATH_TO_EXAMPLE_SRC/.vscode/launch.json"
+	sed -i "s|t_path_to_JLinkScript|$PATH_TO_JLINKSCRIPT|g" "$PATH_TO_EXAMPLE_SRC/.vscode/launch.json"
+	sed -i "s|t_path_tosvd_file|\${workspaceRoot}/.vscode/$SVD_FILE_NAME.svd|g" "$PATH_TO_EXAMPLE_SRC/.vscode/launch.json"
+	sed -i "s|t_path_to_executable|armgcc/\${config:VARISCITE.BUILD_TARGET}/$EXECUTABLE_NAME|g" "$PATH_TO_EXAMPLE_SRC/.vscode/launch.json"
+	sed -i "s|t_soc-cm|$CM_DEVICE_ID|g" "$PATH_TO_EXAMPLE_SRC/.vscode/launch.json"
+	sed -i "s|t_cortex-m-cpu|$CORTEX_M_CPU|g" "$PATH_TO_EXAMPLE_SRC/.vscode/launch.json"
+
+	#raccomandations
+	if [ $CORTEX_M_CPU == "cortex-m4" ] && [ $RAM_TARGET == "ddr" ]; then
+		echo "NOTE! to debug applications mapped in DDR, is mandatory to disable cache (see SystemInit(void) function in....)"
+	fi
+}
+
+while getopts :b:d:e:t:r: OPTION;
+do
+	case $OPTION in
+	b)
+		BOARD_DIR=$OPTARG
+		;;
+	d)
+		GDBSERVER_DIR=$OPTARG
+		;;
+	e)
+		PATH_TO_EXAMPLE_SRC=$OPTARG
+		;;
+	t)
+		RAM_TARGET=$OPTARG
+		;;
+	r)
+		EXECUTABLE_NAME=$OPTARG
+		;;
+	*)
+		usage
+		exit 1
+		;;
+	esac
+done
+
+check_params
+make_vscode
+
+exit 0
