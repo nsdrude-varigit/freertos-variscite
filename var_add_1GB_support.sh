45b29c7f35498d41ae23fdf7cf47bf1078022e38
FrancescoFerraro <francesco.f@variscite.com>
2021-12-07 08:52:02 +0100

    imx8mp-var-dart: completed script to support SOMs with 1GB DDR
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- var_add_1GB_support.sh
+++ var_add_1GB_support.sh
@@ -9,13 +9,13 @@ readonly BSP_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 
 usage()
 {
 	echo
-	echo "This script remap linker files (*.ld) and Vring regions to generate demos running on the"
-	echo "VAR-SOM-MX8M-PLUS and DART-MX8M-PLUS whith 1GB DDR"
+	echo "This script remap the addresses in the linker files (*.ld) to generate M7"
+	echo "  binaries running on the VAR-SOM-MX8M-PLUS and DART-MX8M-PLUS whith 1GB DDR"
 	echo
-	echo " Usage: $0 OPTIONS"
+	echo "Usage: $0 OPTIONS"
 	echo
-	echo " OPTIONS:"
-	echo " -b <dart_mx8mp|som_mx8mp> board folder"
+	echo "OPTIONS:"
+	echo "  -b <dart_mx8mp|som_mx8mp> board folder"
 	echo
 	echo "Examples of use for DART-MX8M-Plus:"
 	echo "  ./${SCRIPT_NAME} -b dart_mx8mp"
@@ -29,16 +29,14 @@ print_wiki_referencies ()
 	echo " - U-Boot: change DDR M7 address and cma size in the enviroment"
 	echo " - Kernel: change M7 device tree file"
 	echo " - FS:     change variscite-rproc.conf file"
-	echo " The detailed guide is provided in the wiki at link: xxxxxx"
-	echo -n "Press (y) to continue or different key to exit: "
-	read key_pressed
-	if [ "$key_pressed" != "y" ]; then
-		exit 1
-	fi
+	echo " The detailed guide is provided in the Variscite Wiki at link:"
+	echo "   https://variwiki.com/index.php?title=MCUXpresso&release=MCUXPRESSO_2.9.0_V1.0_DART-MX8M-PLUS#Add_support_to_SOMs_with_1GB_DDR"
+	echo
 }
 
 check_params()
 {
+	echo
 	if [ "$BSP_BASE_DIR" != "$PWD" ]; then
 		echo "ERROR0: Script must be run from $BSP_BASE_DIR"
 		usage
@@ -46,13 +44,13 @@ check_params()
 	fi
 
 	if [ -z "$BOARD_DIR" ]; then
-		echo "ERROR1: \"board folder is empty"
+		echo "ERROR1: \"board folder is missing"
 		usage
 		exit 1
 	fi
 
 	if [[ ! -d boards/$BOARD_DIR ]] ; then
-		echo "ERROR1: \"boards/$BOARD_DIR\" does not exist"
+		echo "ERROR1: \"boards/$BOARD_DIR\" folder does not exist"
 		usage
 		exit 1
 	fi
@@ -63,7 +61,7 @@ add_1GB_support()
 	case $BOARD_DIR in
 
 	dart_mx8mp|som_mx8mp)
-		echo "Adjust *cm7_*.ld files"
+		echo "Starting remap *cm7_*.ld files"
 		for i in $(find boards/$BOARD_DIR -name "*cm7_ddr_ram.ld"); do
 			sed -i 's/m_interrupts          (RX)  : ORIGIN = 0x80000000, LENGTH = 0x00000400/m_interrupts          (RX)  : ORIGIN = 0x7E000000, LENGTH = 0x00000400/g' "$i"
 			sed -i 's/m_text                (RX)  : ORIGIN = 0x80000400, LENGTH = 0x001FFC00/m_text                (RX)  : ORIGIN = 0x7E000400, LENGTH = 0x001FFC00/g' "$i"
@@ -78,11 +76,12 @@ add_1GB_support()
 		for i in $(find boards/$BOARD_DIR -name "*cm7_flash.ld"); do
 			sed -i 's/m_data2               (RW)  : ORIGIN = 0x80000000, LENGTH = 0x01000000/m_data2               (RW)  : ORIGIN = 0x7E000000, LENGTH = 0x01000000/g' "$i"
 		done
+		echo "Remap completed !"
 		;;
 	esac
 }
 
-while getopts :b:c:d:e:t: OPTION;
+while getopts :b: OPTION;
 do
 	case $OPTION in
 	b)
@@ -95,9 +94,8 @@ do
 	esac
 done
 
-echo "Remember to investigate 0x80000000 in the board.c files !!!!!"
-print_wiki_references
 check_params
 add_1GB_support
+print_wiki_referencies
 
 exit 0
